c31dfb0f361dff2e121eb332edd94d923c42e2f7
antran2704 <phamtrangiaan27@gmail.com>
2022-05-12 14:45:13 +0700

    up code

---
+++ src/components/Global/index.js
@@ -0,0 +1,9 @@
+import './GlobalStyle.scss'
+
+const Global = ({children}) => {
+    return ( 
+        children
+     );
+}
+ 
+export default Global;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Global from './components/Global/index.js';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Global>
+      <App />
+    </Global>
   </React.StrictMode>
 );
 
